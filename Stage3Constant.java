74b4faf680048dd680617c7342c6600f43bac7cd
wns7327 <81146574+wns7327@users.noreply.github.com>
2021-05-23 14:43:07 +0900

    Add files via upload

---
+++ Stage3Constant.java
@@ -0,0 +1,18 @@
+package stage3;
+
+public interface Stage3Constant {
+
+	static int BALL_WIDTH = 20; 
+	static int BALL_HEIGHT = 20;
+	static int BLOCK_ROWS = 5;
+	static int BLOCK_COLUMNS = 10; 
+	static int BLOCK_WIDTH = 40; 
+	static int BLOCK_HEIGHT = 20; 
+	static int BLOCK_GAP = 3; 
+	static int BAR_WIDTH = 80; 
+	static int BAR_HEIGHT = 20; 
+	static int CANVAS_WIDTH = 400;
+	static int CANVAS_HEIGHT = 600;
+}
+
+
